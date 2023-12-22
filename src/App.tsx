d0442cbd232b1b532e1469cc26a9c6936804a1d0
Ogun Akar <ogunakar09@gmail.com>
2023-12-23 00:10:55 +0300

    feat:fetch film data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,34 @@
-import React from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import Table from "./Table";
 import "./App.scss";
+import { BASE_URL } from "./utilities/constants";
 
 function App() {
+  const [data, setData] = useState<any[]>([]);
+
+  const fetchFilms = useCallback(async () => {
+    try {
+      const API_KEY = process.env.REACT_APP_API_KEY;
+      const FETCH_URL = `${BASE_URL}${API_KEY}&tt1285016`;
+
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${API_KEY}&t=Pokemon`
+      );
+      const data = await response.json();
+      setData(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+
+  useEffect(() => {
+    fetchFilms();
+  }, []);
+
+  console.log("data", data);
+
   return (
     <div className="App">
       {/* <header className="App-header">
