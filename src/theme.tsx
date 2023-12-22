3affb8cb3006c07f04d561281a9745e26979e00c
Ogun Akar <ogunakar09@gmail.com>
2023-12-22 22:31:15 +0300

    feat: scss and mui package setup

---
+++ src/theme.tsx
@@ -0,0 +1,16 @@
+// // When using TypeScript 4.x and above
+// import type {} from "@mui/x-data-grid/themeAugmentation";
+// import type {} from "@mui/x-data-grid-pro/themeAugmentation";
+// import type {} from "@mui/x-data-grid-premium/themeAugmentation";
+
+// const theme = createTheme({
+//   components: {
+//     MuiDataGrid: {
+//       styleOverrides: {
+//         root: {
+//           backgroundColor: "red",
+//         },
+//       },
+//     },
+//   },
+// });
