5b4af86b1088d87510754d6024d128bdce0f9e8b
Ogun Akar <ogunakar09@gmail.com>
2023-12-24 09:23:16 +0300

    feat: add styles setup for details page

---
+++ src/components/details/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+const Details = () => {
+  return (
+    <div className="details">
+      <span>Details</span>
+    </div>
+  );
+};
+
+export default Details;
